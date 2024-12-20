6c3684d2551b1f41fd20faf2f0a5d156124226f2
phuravongpech <phuravong17@gmail.com>
2024-12-20 12:47:16 +0700

    es module fix 2

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-import { assert } from 'chai';
-import app from '../app';
+const assert = require('chai').assert;
+const app = require('../app.js');
 describe('Simple test', function() {
  it('should return hello message', function() {
  assert.equal('Hello Teacher', 'Hello, Teacher');
