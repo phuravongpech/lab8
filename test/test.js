b8c0a3e82f3128fb5a0e9a9e4f0506063670ad2b
phuravongpech <phuravong17@gmail.com>
2024-12-20 12:27:34 +0700

    hello teacher

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const assert = require('chai').assert;
 const app = require('../app');
 describe('Simple test', function() {
  it('should return hello message', function() {
- assert.equal('Hello, GitHub Actions!', 'Hello, GitHub Actions!');
+ assert.equal('Hello Teacher', 'Hello, Teacher');
  });
 });
